6fb003a248d24d4dc4c42ad30788abad378bc3ba
d. <daisuketanigawa@live.fr>
2025-01-16 21:55:10 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Launch monitoring.sh => generate output
-bash /home/wall_monitoring/monitoring.sh > output
+bash monitoring.sh > output
 
 # Broadcast output using wall
 wall -t 15 output
